30b9e18abafef174775e898e0ad7e186f11f4ba0
Stephen Yang <stephy.yang@mail.utoronto.ca>
2021-10-31 18:38:42 -0400

    wrote code to save data splits.

--- data/dataset_splitting.py
+++ data/dataset_splitting.py
@@ -4,6 +4,7 @@ import multiprocessing
 import pandas as pd
 import numpy as np
 import shutil
+import csv
 import os
 
 
@@ -20,6 +21,11 @@ class Data_Splitting():
         self.df = self.load_csv()
         self.classes = self.create_dict(self.df)
         self.num_classes = len(self.classes.items())
+
+        # Enable multiprocessing
+        self.mp = False
+
+
 
         # print((self.classes[1][0]["id"]))
 
@@ -40,20 +46,34 @@ class Data_Splitting():
         grouped = df.set_index("landmark_id").groupby(level=0)
         return dict(grouped.apply(lambda x: x.to_dict(orient='records')))
 
-    def create_split(self,output_path:str):
-        print("Creating split with {} workers and saving to \"{}\"".format(num_workers,output_path))
+    def create_split(self):
         
         # Create directory if does not exist
-        if not(os.path.exists(output_path)):
-            os.makedirs(output_path)
+        if not(os.path.exists(self.out_path)):
+            os.makedirs(self.out_path)
 
         # Save 1 image from every class
-        save_indices = [i for i in range(0,self.num_classes)]
+        save_indices = self.classes.keys() #self.num_classes)]
 
-        with multiprocessing.Pool(processes=num_workers) as pool:
-            data = pool.map(save_ex)
+        print(self.classes.keys())
 
-    def save_ex(self,lm_index:int,output_path,img_index:int=0):
+        if(self.mp):
+            print("Creating split with {} workers and saving to \"{}\"".format(num_workers,output_path))
+
+            with multiprocessing.Pool(processes=int(self.num_workers)) as pool:
+                data = pool.map(self.save_ex,save_indices)
+
+                self.save_csv(data)
+        else:
+            print("Creating split with 1 worker and saving to \"{}\"".format(output_path))
+
+            data = []
+            for i in save_indices:
+                data = self.save_ex(i)
+
+            self.save_csv(data)
+
+    def save_ex(self,lm_index:int,img_index:int=0):
 
         # Get the image id of the first landmark with landmark_id = id
         lm_id = self.get_lm_from_id(lm_index=lm_index,im_index=img_index)
@@ -62,7 +82,7 @@ class Data_Splitting():
         img_path = self.resolve_img_path(lm_id)
         img_dir = self.resolve_img_dir(lm_id)
 
-        full_dir = os.path.join(output_path,img_dir)
+        full_dir = os.path.join(self.out_path,img_dir)
 
         # Get image from full dataset and save image to new location
         if not(os.path.exists(full_dir)):
@@ -73,21 +93,27 @@ class Data_Splitting():
         # Add image id to list of image id's in new dataset
         return ({lm_id: lm_index})
 
+    def save_csv(self,data_dict):
 
-    #         files = os.listdir('.')
-    # file_list = [filename for filename in files if filename.split('.')[1]=='csv']
+        # Save csv location
+        csv_path = os.path.join(self.out_path,"train.csv")
 
-    # # set up your pool
-    # with Pool(processes=8) as pool: # or whatever your hardware can support
+        header = ["id","landmark_id"]
 
-    #     # have your pool map the file names to dataframes
-    #     df_list = pool.map(read_csv, file_list)
+        with open(csv_path,"w+") as f:
+            writer = csv.writer
+            writer.writerow(header)
 
-    #     # reduce the list of dataframes to a single dataframe
-    #     combined_df = pd.concat(df_list, ignore_index=True)
+            # Loop through examples in data_dict and save dict to csv
+            for ex in data_dict:
+                print(ex)
+                for k, v in ex.items():
+                    writer.writerow([k,v])
 
+        
     def get_lm_from_id(self,lm_index:int,im_index:int=0):
         # Gets a image index (im_index) from a given landmark index (lm_index)
+        print(lm_index,im_index)
         if(im_index>len(self.classes[lm_index])):
             raise ValueError("im_index exceeds # of images with this landmark id!")
         return self.classes[lm_index][im_index]["id"]
@@ -111,10 +137,10 @@ if(__name__=="__main__"):
     output_path = cfg["OUTPUT_PATH"]
 
     # Create data_splitting object with dataset at dataset_path and num_workers
-    d = Data_Splitting(dataset_path,num_workers)
+    d = Data_Splitting(dataset_path,output_path,num_workers)
 
     # Split data to a smaller dataset saved at output_path and with N samples
-    # d.create_split(output_path)
+    d.create_split()
     
 
 
