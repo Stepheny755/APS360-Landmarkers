20b742c8fb9df68ac2e7ccfd987b1cf6fe277cf3
Syed-Talha <56983763+Syed-Talha@users.noreply.github.com>
2021-11-28 13:27:30 -0500

    updated with resnet embedding

--- retrieval.py
+++ retrieval.py
@@ -1,30 +1,23 @@
 import torch.nn.functional as F
 
-def get_knn(query, models, train_loader, k=3):
+def get_knn_effnet(query, effnet, train_loader, k=3):
     #TODO: ADD GPU SUPPORT TO THIS 
     """
-    Takes in a query vector, feeds it into the trained models, and retrieves its k-nearest neighbours
-    measured by cosine similarity from EACH of the three models. The nearest neighbours are obtained 
+    Takes in a query vector, feeds it into effnet, and retrieves its k-nearest neighbours
+    measured by cosine similarity. The nearest neighbours are obtained 
     from the training dataset without any extra augmented points.
 
     Input Arguments:
 
     query: a query image that has been preprocessed and can be fed into the models
         must have dimensions 1 x 3 x 224 x 224 (unsqueeze if needed)
-    models: a list/iterable of the trained models, where
-        models[0]: EfficientNet
-        models[1]: SENet
-        models[2]: swin transformer
-        models[3]: DeLF + SVM
+    effnet: an EfficientNet model object
     train_loader: the DataLoader object with all the training data in it
     k: the number of nearest neighbours (duh)
 
     Output Arguments:
 
     eff_net_matches: list of top k matches from EfficientNet
-    se_net_matches: list of top k matches from SENEt
-    swin_matches: list of top k matches from Swin Transformer
-    delf_matches: list of top k matches from DeLF + SVM
     """
 
     #need to iterate through training set and store embeddings of each training example 
@@ -33,11 +26,11 @@ def get_knn(query, models, train_loader, k=3):
     for idx, (image, label) in enumerate(train_loader):
         batch_size = image.shape[0]
         for i in range(batch_size):
-            effnet_image_list.append(models[0](image[i].unsqueeze(0)))
+            effnet_image_list.append(effnet.embedding(image[i].unsqueeze(0)))
 
 
     #EfficientNet
-    query_eff_net = models[0](query) #embedding of query from EfficientNet
+    query_eff_net = effnet.embedding(query) #embedding of query from EfficientNet
     eff_net_matches = []
     eff_net_dist_scores = {}
 
@@ -51,4 +44,49 @@ def get_knn(query, models, train_loader, k=3):
         eff_net_matches.append(effnet_image_list[match_indices[l]])
 
     return eff_net_matches 
+
+def get_knn_resnet(query, resnet, train_loader, k=3):
+    #TODO: ADD GPU SUPPORT TO THIS 
+    """
+    Takes in a query vector, feeds it into resnet, and retrieves its k-nearest neighbours
+    measured by cosine similarity. The nearest neighbours are obtained 
+    from the training dataset without any extra augmented points.
+
+    Input Arguments:
+
+    query: a query image that has been preprocessed and can be fed into the models
+        must have dimensions 1 x 3 x 224 x 224 (unsqueeze if needed)
+    resnet: an SE ResidualNet model object
+    train_loader: the DataLoader object with all the training data in it
+    k: the number of nearest neighbours (duh)
+
+    Output Arguments:
+
+    eff_net_matches: list of top k matches from EfficientNet
+    """
+
+    #need to iterate through training set and store embeddings of each training example 
+    #if already done, remove this
+    resnet_image_list = []
+    for idx, (image, label) in enumerate(train_loader):
+        batch_size = image.shape[0]
+        for i in range(batch_size):
+            resnet_image_list.append(resnet.embedding(image[i].unsqueeze(0)))
+
+
+    #EfficientNet
+    query_res_net = resnet.embedding(query) #embedding of query from SE ResNet
+    res_net_matches = []
+    res_net_dist_scores = {}
+
+    for j in range(len(resnet_image_list)):
+        if j == 1:
+            print(resnet_image_list[j].shape)
+        res_net_dist_scores[j] = float(F.cosine_similarity(query_res_net, resnet_image_list[j]))
+    match_indices = sorted(res_net_dist_scores, key=res_net_dist_scores.get, reverse=True)[:k]
+
+    for l in range(k):
+        res_net_matches.append(resnet_image_list[match_indices[l]])
+
+    return res_net_matches
 
